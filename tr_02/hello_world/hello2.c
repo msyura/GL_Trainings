8d95116b2d44383d38114441dfa84c7331515d5c
Yuriy Mysan <yuramysan@gmail.com>
2017-09-11 11:53:53 +0300

    Added hello2 module input parameter.

--- tr_02/hello_world/hello2.c
+++ tr_02/hello_world/hello2.c
@@ -1,6 +1,8 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/printk.h>
+#include <linux/types.h>
 #include "hello1.h"
 
 MODULE_AUTHOR("Yuriy Mysan <yuramysan@gmail.com");
@@ -9,14 +11,33 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 extern void print_hello(void);
 
+static uint counter = 1;
+module_param(counter, uint, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(counter, "uint counter");
+
+
 void print_goodbye(void) 
 {
     printk(KERN_EMERG "Good bye!\n");
 }
 
 static int __init hello_init(void)
-{    
-    print_hello();
+{   
+    BUG_ON(counter > 10);
+    WARN_ON(counter == 0);
+
+    if (counter == 5) 
+    {
+	return -EINVAL;
+    }
+    else if (counter > 0 && counter < 10) 
+    {
+        uint i = 0;
+        for (i = 0; i < counter; i++)
+        {
+            print_hello();
+        }
+    }
     return 0;
 }
 
